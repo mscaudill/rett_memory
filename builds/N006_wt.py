37fe39c2178ecd964b62eb9d64f9485b6b01a961
mscaudill <mscaudill@gmail.com>
2021-10-27 16:57:55 -0500

    adding builds a temp file

---
+++ builds/N006_wt.py
@@ -0,0 +1,10 @@
+import pickle
+import numpy as np
+
+from scripting.rett_memory._rett_recall import casia
+
+#GLOBALS
+BASE = '/media/matt/Zeus/Lingjie/data/N006_wt_female_mcorrected/'
+
+# Here just compute the basis images and the source images for the training
+# and combine with the already existing data
