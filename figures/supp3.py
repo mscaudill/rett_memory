f4327a2fc882e1f0c2aec0a8cc7f39100f03c4be
mscaudill <mscaudill@gmail.com>
2021-10-29 16:40:27 -0500

    docs for figures

--- figures/supp3.py
+++ figures/supp3.py
@@ -66,7 +66,7 @@ def plot_source_mip():
 
 def plot_mipsource_rois(cells=[13, 32, 174, 145], annulus_cell=174):
     """Plots the max intensity prijection  of the source images and
-    a selection of cell rois.
+    a selection of cell rois for a zoomed region.
 
     The source images are computed from the entire recording session (i.e.
     across all environmental contexts.
@@ -76,23 +76,28 @@ def plot_mipsource_rois(cells=[13, 32, 174, 145], annulus_cell=174):
     mip = np.max(data, axis=0)
     fig, ax = plt.subplots()
     ax.imshow(mip, cmap='gray')
+    ax.set_xlim(260, 360)
+    ax.set_ylim(270, 350)
+    ax.invert_yaxis()
     with open(paths.data.joinpath('N006_wt_rois.pkl'), 'rb') as infile:
         rois = pickle.load(infile)
     #plot the cell boundaries and annuli
-    for cell in cells:
+    for cell in range(len(rois['boundaries'])):
         boundary = rois['boundaries'][cell]
         annulus = rois['annuli'][cell]
         if cell == annulus_cell:
-            ax.plot(boundary[:,1], boundary[:,0], color='orange')
-            ax.plot(annulus[:,1], annulus[:,0], linestyle=' ', marker='o',
+            ax.plot(boundary[:,1], boundary[:,0], color='orange',
+                    linewidth=2)
+            ax.plot(annulus[:,1], annulus[:,0], linestyle=' ', marker='s',
                 color='b')
         else:
-            ax.plot(boundary[:,1], boundary[:,0], color='green')
+            ax.plot(boundary[:,1], boundary[:,0], color='green', linewidth=2)
     return ax
 
 def plot_cxt_mips(cxts=['Train', 'Fear', 'Neutral', 'Fear_2']):
-    """ 
+    """Plots the source images computed from each context independently. 
 
+    The source images are computed indepently for each context.
     """
 
     path = paths.data.joinpath('N006_wt_cxtsources.pkl')
@@ -106,8 +111,9 @@ def plot_cxt_mips(cxts=['Train', 'Fear', 'Neutral', 'Fear_2']):
     return axarr
 
 def plot_cxt_mips_zoom(cxts=['Train', 'Fear', 'Neutral', 'Fear_2']):
-    """
-
+    """Plots a zoomed region of the context source images along with the
+    boundaries of the detected ROIs derived from the source images across
+    the entire recording session.
     """
 
     path = paths.data.joinpath('N006_wt_cxtsources.pkl')
@@ -115,21 +121,19 @@ def plot_cxt_mips_zoom(cxts=['Train', 'Fear', 'Neutral', 'Fear_2']):
         data = pickle.load(infile)
     mips = {name: np.max(sources, axis=0) for name, sources in data.items()}
     fig, axarr = plt.subplots(1, len(cxts), sharex=True, sharey=True)
-
-
+    # load the roi boundaries and annuli
     with open(paths.data.joinpath('N006_wt_rois.pkl'), 'rb') as infile:
         rois = pickle.load(infile)
-
+    # plot the zoomed context sources
     for cxt, ax in zip(cxts, axarr):
         ax.imshow(mips[cxt], cmap='gray')
         ax.set_xlim(325, 425)
         ax.set_ylim(385, 485)
         ax.invert_yaxis()
-
+        # plot the roi boundaries
         for cell in range(len(rois['boundaries'])):
             boundary = rois['boundaries'][cell]
             ax.plot(boundary[:,1], boundary[:,0], color='green')
-
     fig.tight_layout()
     return axarr
 
@@ -142,9 +146,9 @@ if __name__ == '__main__':
     #axarr = plot_basis()
     #axarr = plot_sources()
     #ax = plot_source_mip()
-    #plot_mipsource_rois()
+    plot_mipsource_rois()
     
-    axarr = plot_cxt_mips() 
+    #axarr = plot_cxt_mips() 
     #axarr = plot_cxt_mips_zoom() 
 
     plt.show()
