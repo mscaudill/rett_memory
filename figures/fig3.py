e50ed12ef3d7b26810b1ca2579a710ed0937a202
mscaudill <mscaudill@gmail.com>
2021-11-12 12:35:35 -0600

    initial commit of fig3 revision

---
+++ figures/fig3.py
@@ -0,0 +1,54 @@
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+from scripting.rett_memory import paths
+from scripting.rett_memory.metrics import activity, behavior
+from scripting.rett_memory.tools import plotting, pdtools
+
+DATAPATH = paths.data.joinpath('signals_df.pkl')
+BEHAVIORPATH = paths.data.joinpath('behavior_df.pkl')
+RASTER_EXPS = [('wt','N124', 'NA'), ('het', 'N229', 'NA')]
+RASTER_CXTS = ['Fear_2'] * 2
+
+
+def rasters(exps=RASTER_EXPS, cxts=RASTER_CXTS):
+    """Constructs the raseters and network activity traces of panels A and
+    B for Figure 3 of the paper."""
+
+    #read in dataframes of signals and behaviors
+    df = pd.read_pickle(DATAPATH)
+    df = df.sort_index()
+    bdf = pd.read_pickle(BEHAVIORPATH)
+    activities = activity.NetworkActivity(df, std=5).measure()
+    fig, axarr = plt.subplots(2, 2, figsize=(8,3), sharex=True)
+    for idx, (exp, cxt) in enumerate(zip(exps, cxts)):
+        #get plot position coords
+        pos = np.unravel_index(idx, axarr.shape)
+        spike_indices = df.loc[exp][cxt + '_spikes']
+        #grab the first 15 cells spike indices
+        for row, subls in enumerate(spike_indices[0:15]):
+            spike_times = np.array(subls) / df.iloc[0].sample_rate
+            axarr[pos].scatter(spike_times, row * np.ones(len(spike_times)),
+                               marker='|', s=5, color='k')
+        trace = activities.loc[exp][cxt]
+        #place activity trace at top of plot and scale it
+        axarr[pos].plot(np.linspace(0, 300, 6000), 3 * trace + 16)
+        #get the freezing signal and threshold to remove freezes < 2 secs
+        btimes = bdf.loc[exp][cxt + '_time']
+        freezes = behavior.threshold(bdf.loc[exp][cxt + '_freeze'], 
+                                     btimes, 2)
+        axarr[1, idx].plot(btimes, freezes)
+
+    # FIXME 
+    # ADD THRESHOLD
+    # ADD SCALEBAR
+
+    fig.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+
+    rasters()
