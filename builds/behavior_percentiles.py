d76be57aff3990d1f20fd65be89ad91fa00da47c
mscaudill <mscaudill@gmail.com>
2021-12-05 14:39:46 -0600

    build of som 80th percentile mice

--- builds/behavior_percentiles.py
+++ builds/behavior_percentiles.py
@@ -9,14 +9,17 @@ def build_P80_animals(df, context='Fear_2', min_time=1):
     whose freezing percentage is greater than the 20th percentile."""
 
     idxs = filter_freezes(df, [context], min_time, qs=[0.2])[0.2]
-    path = paths.data.joinpath('P80_animals.pkl')
+    path = paths.data.joinpath('P80_som_animals.pkl')
     with open(path, 'wb') as outfile:
         pickle.dump(idxs, outfile)
+    return idxs
 
 
 if __name__ == '__main__':
 
-    df = pd.read_pickle(paths.data.joinpath('behavior_df.pkl'))
-    build_P80_animals(df)
+    #df = pd.read_pickle(paths.data.joinpath('behavior_df.pkl'))
+    som_df = pd.read_pickle(paths.data.joinpath('som_behavior_df.pkl'))
+    som_df = som_df.drop(('ssthet', 'sn554',))
+    mice = build_P80_animals(som_df)
         
 
